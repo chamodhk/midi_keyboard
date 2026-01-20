339c261d687e6b6febb5de4cc909e1144d5ca392
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2026-01-20 20:36:52 +0530

    updated manufacturer name and the device name

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -145,8 +145,8 @@ enum {
 // array of pointer to string descriptors
 static char const *string_desc_arr[] = {
   (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-  "TinyUSB",                     // 1: Manufacturer
-  "TinyUSB Device",              // 2: Product
+  "Chamodh Nethsara",                     // 1: Manufacturer
+  "MIDI Keyboard",              // 2: Product
   NULL,                          // 3: Serials will use unique ID if possible
 };
 
